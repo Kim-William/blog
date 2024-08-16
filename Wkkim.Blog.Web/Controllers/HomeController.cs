f6cf9f35bf0d58eceb0fe7af241e20538a79ad7d
Kim-William <130305118+Kim-William@users.noreply.github.com>
2024-08-16 17:19:39 +0200

    Added Contact, About Pages
    now you can contact me through the contact page

--- Wkkim.Blog.Web/Controllers/HomeController.cs
+++ Wkkim.Blog.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,14 +18,21 @@ namespace Wkkim.Blog.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
         private readonly ITagRepository tagRepository;
+        private readonly IConfiguration configuration;
+        private readonly string email_id;
+        private readonly string email_password;
 
         public HomeController(ILogger<HomeController> logger,
             IBlogPostRepository blogPostRepository,
-            ITagRepository tagRepository)
+            ITagRepository tagRepository,
+            IConfiguration configuration)
         {
             _logger = logger;
             this.blogPostRepository = blogPostRepository;
             this.tagRepository = tagRepository;
+            this.configuration = configuration;
+            email_id = configuration.GetSection("EMail")["ID"];
+            email_password = configuration.GetSection("EMail")["Password"];
         }
 
         public async Task<IActionResult> Index()
@@ -46,6 +55,54 @@ namespace Wkkim.Blog.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Contact")]
+        public async Task<IActionResult> Contact(ContactInfo query)
+        {
+            SendEmail(query);
+            return View();
+        }
+
+        public void SendEmail(ContactInfo query)
+        {
+            // 이메일 설정
+            try
+            {
+                string smtpAddress = "smtp.gmail.com";
+                int portNumber = 587;
+                bool enableSSL = true;
+                string subject = $"New Contact Request from Wkkim Blog";
+                string body = $"Thank you for contacting me.<br>I will personally review this email and respond shortly.<br>Below are the name, email, and message of the person who requested to contact me.<br>Name: {query.Name}<br>Email: {query.EMail}<br>Message:{query.Message}";
+
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(email_id);
+                    mail.To.Add(email_id);
+                    mail.To.Add(query.EMail);
+                    mail.Subject = subject;
+                    mail.Body = body;
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                    {
+                        smtp.Credentials = new NetworkCredential(email_id, email_password);
+                        smtp.EnableSsl = enableSSL;
+                        smtp.Send(mail);
+                    }
+                }
+                ViewBag.Message = "Thank you! Email sent successfully!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = $"Error: {ex.Message}";
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Wkkim.Blog.Web/Models/ViewModels/ContactInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Wkkim.Blog.Web.Models.ViewModels
+{
+    public class ContactInfo
+    {
+        public string Name { get; set; }
+        public string EMail { get; set; }
+        public string Message { get; set; }
+    }
+}
